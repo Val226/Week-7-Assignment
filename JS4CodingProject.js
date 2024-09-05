6f8625aa8668562e0b6d0446424040067e68b092
Val226 <valboddy@gmail.com>
2024-09-05 15:37:42 -0600

    Update JS4CodingProject.js
    Array name change

--- JS4CodingProject.js
+++ JS4CodingProject.js
@@ -1,6 +1,6 @@
 // Create an array called ages that contains the following values: 3, 9, 23, 64, 2, 8, 28, 93.
 
-let array = [3, 9, 23, 64, 2, 8, 28, 93];
+let ages = [3, 9, 23, 64, 2, 8, 28, 93];
 
 // Programmatically subtract the value of the first element in the array from the value in the last element of the array.
 // Do not use numbers to reference the last element, find it programmatically.
